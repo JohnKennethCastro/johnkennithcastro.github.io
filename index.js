992f647f87553f7427fdeb9b7c61cd5c4b6f3a41
JohnKennethCastro <jkbcastro25@gmail.com>
2024-07-06 08:44:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -77,6 +77,18 @@ function calculateChange() {
     }
 }
 
+function pay() {
+
+    carts.textContent = "";
+
+    total.value = "";
+    cash.value = "";
+    change.value = "";
+
+    alert(" Thanks! So excited for your new Laptop Arrived");
+}
+
+
 qty1.addEventListener("keyup", addOrder);
 qty2.addEventListener("keyup", addOrder);
 qty3.addEventListener("keyup", addOrder);
